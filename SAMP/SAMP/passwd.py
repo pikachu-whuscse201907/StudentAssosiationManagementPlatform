c2cae499a5a4ae828700e8ed739ef17d20f9e26e
1138164561 <1138164561@qq.com>
2019-07-18 20:08:43 +0800

    20190718-2008-lijw. Change password.

--- SAMP/SAMP/passwd.py
+++ SAMP/SAMP/passwd.py
@@ -1,23 +1,53 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
-from . import view, view_special
-from people.models import Person, Organizations, ClubAnnouncements, User_info, MembershipApplication
-from .database import function, search
+from . import view
+from .database import function, search, delete
+
 
 def passwd(request):
-    content = {}
+    context = {}
     cookie_id = request.COOKIES.get('id', None)
-    if cookie_id is None:
-        return view.response_not_logged_in(request)
-    
-    result = function.get_user_info(cookie_id)  # call database
+    result = function.get_user_info(cookie_id)
     if not result['success']:
         return view.response_not_logged_in(request)
+
     info = result['info']
-    content['name'] = info['user_name']
-    return HttpResponse(render(request,'passwd.html',content))
+    context['name'] = info['user_name']
+    context['user_logo'] = info['user_logo']
+    
+    name = request.POST.get('name', None)
+    rawpsd = request.POST.get('rawpsd', None)
+    newpsd = request.POST.get('newpsd', None)
+    cnewpsd = request.POST.get('cnewpsd', None)
+
+    if name is None or rawpsd is None or newpsd is None or cnewpsd is None:
+        return HttpResponse(render(request, 'passwd.html', context))
+
+    if name is not None and not search.pswd_correct(name, rawpsd):
+        delete.delete_cookie(cookie_id)
+    else:
+        if info['user_name'] == name and newpsd == cnewpsd:
+            user = search.user_of_username(name)
+            user.pswd = newpsd
+            user.save()
+            context['title'] = '修改密码成功'
+            context['url'] = '../userpage/'
+            context['error_msg'] = '修改密码成功'
+            return HttpResponse(render(request, 'jump.html', context))
+
+    return HttpResponse(render(request, 'passwd.html', context))
+
 
 def checkpsd(request):
-    name = request.POST.get('name')
-    pswd = request.POST.get('pswd')
+    cookie_id = request.COOKIES.get('id', None)
+    result = function.get_user_info(cookie_id)
+    if not result['success']:
+        return HttpResponse("False")
+    info = result['info']
+    name = request.POST.get('name', None)
+    pswd = request.POST.get('pwsd', None)
+    if name is not None and pswd is not None:
+        if info['user_name'] == name and search.pswd_correct(name, pswd):
+            return HttpResponse("True")
+    
     return HttpResponse("False")
