d280f9e72cc3467bd778e32a29335d940d204ba4
1138164561 <1138164561@qq.com>
2019-07-19 11:30:10 +0800

    20190719-1129-lijw. Bugs in clubactivities and myactivity, no actual data.

--- SAMP/SAMP/activity.py
+++ SAMP/SAMP/activity.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 import datetime
-from .database import function
+from .database import function, search
 from . import view
+from people.models import Person, MembershipApplication, Organizations, Activ
 
 
 def clubactivities(request):
@@ -15,8 +16,35 @@ def clubactivities(request):
     context['islogin'] = True
     context['name'] = info['user_name']
     context['user_logo'] = info['user_logo']
+
+    org_name = request.GET.get('iden', None)
+    org = Organizations.objects.filter(organization_name=org_name)
+    if 0 == len(org):
+        context['title'] = 'Cannot find the club.'
+        context['url'] = '../searchclub/'
+        context['error_msg'] = 'Cannot find a club named "' + org_name + '".'
+        response = HttpResponse(render(request, 'jump.html', context))
+        return response
+
+    user_info = search.user_info_of_username(info['user_name'])
+    if user_info not in org[0].members.all():
+        context['title'] = 'Not Authorized.'
+        context['url'] = '../clubpage/?iden=' + org_name
+        context['error_msg'] = 'Only club member can see the members.'
+        response = HttpResponse(render(request, 'jump.html', context))
+        return response
+
+    if info['user_name'] == org[0].master.name.name:
+        context['ismanager'] = True
+    else:
+        context['ismanager'] = False
+
+    context['org_name'] = org_name
+    context['org_logo'] = org[0].org_logo
     
     context['activities'] = []
+    context['hasactivities'] = (0 < len(context['activities']))
+    
     response = HttpResponse(render(request, 'clubactivities.html', context))
     return response
 # Ending of function clubactivities(request)
@@ -33,6 +61,11 @@ def myactivity(request):
     context['name'] = info['user_name']
     context['user_logo'] = info['user_logo']
     
+    context['activities'] = []
+    context['hasactivities'] = (0 < len(context['activities']))
+    
+    context['joinedactivities'] = []
+    context['hasjoinedactivities'] = (0 < len(context['joinedactivities']))
     
     response = HttpResponse(render(request, 'myactivity.html', context))
     return response
